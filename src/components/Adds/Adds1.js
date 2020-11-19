ceb2fdb383982d0ec7f17dd13df9dcb12ced9750
HuzaifaKhan20 <57367781+HuzaifaKhan20@users.noreply.github.com>
2020-11-19 19:08:48 +0500

    Add files via upload

---
+++ src/components/Adds/Adds1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import VivoMobile from '../images/vivombl.jpg';
+import "./Adds.css"
+
+function Adds1() {
+    return (
+        <div>
+
+            <center><a target="_blank" href="https://www.tecno-mobile.com/pk/phones/product-detail/product/camon-16-5/?gclid=Cj0KCQjwreT8BRDTARIsAJLI0KJVtZzdkl4BE1Y_DoJwY3miN3QSX7990KFwVp8Cq-ugzg8Ga1j659QaAk72EALw_wcB#/"><img src={VivoMobile} className="img-fluid adds__container" alt="..." /></a></center>
+
+        </div>
+
+    )
+}
+
+export default Adds1;
